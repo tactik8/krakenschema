25f08f4e903e1a9307c4e22ceea0d01192b24072
tactik8 <admin@tactik8.com>
2020-09-16 13:10:16 +0000

    go

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='krakenschema2',
+    name='krakenschema',
     version='0.1.2',
     description='kraken schema module',
     author='Tactik8',
     author_email='admin@tactik8.com',
-    packages=['krakenschema2'],  #same as name
+    packages=['krakenschema'],  #same as name
     install_requires=['requests'], #external packages as dependencies
 )
